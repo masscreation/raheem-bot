a8a89871af8b028ea3e00c10a6220e77803f3ff3
Zac Rolland <zrolland@gmail.com>
2016-08-26 13:07:17 -0700

    started basic convo engine

--- utils/convoEngine.js
+++ utils/convoEngine.js
@@ -1,11 +1,9 @@
 'use strict'
 
 const content = require('../content');
-const stateMachine = require('./stateMachine');
+const state = require('./stateMachine');
 const store = require('./store')
 
-const initialState = "STEP:1_GET_STARTED_PAYLOAD";
-let currentState = initialState;
 
 /*
  * Collection of methods to managing the convo
@@ -15,13 +13,38 @@ module.exports = {
 
   // 1. Consult the conversation state
   // 2. Check if the user message is valid for this state
-  // 3. Get the next piece of content
-  // 4. Send out next piece of content
+  // 3. Process user message if necessary
+  // 4. Get the next piece of content
+  // 5. Send out next piece of content
+  //    a. define & format content-out components
 
-  send(recipientID, message) {
-    console.log("message received that could be sent back: ", message);
-    stateMachine.nextState();
-    router.out(recipientID, message);
+  send(incomingPayload) {
+
+    if (typeof incomingPayload !== "string") {
+			return Promise.reject(new Error('message is not a string'));
+		}
+
+		return new Promise(function(resolve, reject) {
+
+      if (incomingPayload) {
+        console.log("incomingPayload: ", incomingPayload);
+        state.next();
+
+        let currentState = state.get();
+        console.log("STATE: ", currentState);
+        // this is where the smarts is gonna happen, do we call a script
+        // or send to wit.ai  or something else
+        // what do we send back: text, objects, etc.
+
+        let outgoingObj = content[currentState];
+        console.log("Outgoing Object: ", outgoingObj);
+        resolve(outgoingObj);
+
+      } else {
+        reject(new Error('something went wrong in the convoEngine'))
+      }
+
+    });
   }
 
 };
