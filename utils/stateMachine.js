155a0747d7e7a67b9e3062e2995f5aa09dc0dfb6
Zac Rolland <zrolland@gmail.com>
2016-08-26 13:08:21 -0700

    refactor

--- utils/stateMachine.js
+++ utils/stateMachine.js
@@ -1,14 +1,22 @@
 'use strict'
 
+const content = require('../content');
+const initialState = "STEP:1_GET_STARTED_PAYLOAD";
+let currentState = initialState;
+
 /*
  * Collection of methods related to identifying where the user 'is' in the
  * flow of conversation with the bot
  */
 module.exports = {
 
-  nextState() {
+  next() {
     currentState = content[currentState].nextMessage;
     console.log("currentState: ", currentState);
+  },
+
+  get() {
+    return currentState;
   }
 
 };
