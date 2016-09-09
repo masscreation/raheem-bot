bc645f7a845dd18be0abeba8b18cad3dfe2a2dd0
Jordan Lawler <Jordanalawler@gmail.com>
2016-09-08 17:00:47 -0700

    fixed minor typos

--- utils/stateMachine.js
+++ utils/stateMachine.js
@@ -40,6 +40,8 @@ module.exports = {
       currentState = stateStore[lastStateIndex - 2];
     } else if (message.toLowerCase() === "restart"){
       currentState = stateStore[0];
+    } else if (message.toLowerCase() === "new report"){
+      currentState = "STEP:RESTART_CONVO";
     }
     console.log("GET STATE", currentState)
   },
