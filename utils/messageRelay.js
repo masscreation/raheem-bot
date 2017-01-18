c644cb0985c3ac158a05c027b72f3c362bebab13
Jordan Lawler <Jordanalawler@gmail.com>
2017-01-17 16:30:14 -0800

    added error handling

--- utils/messageRelay.js
+++ utils/messageRelay.js
@@ -4,8 +4,12 @@ const MessageService = require('./messageService');
 const MessageCourier = require('./messageCourier');
 const AnalyticsApi = require('./services/analyticsAppApi');
 const SeedAppService = require('./services/seedAppApi');
-
 const async = require('async');
+const Raven = require('raven');
+
+Raven.config('https://e5eb226eff9f453cb8aaaf130bcb8a62:601a1c12a6714c86ab502f432f6002a8@sentry.io/130176', {
+  captureUnhandledRejections: true
+}).install();
 
 module.exports = function(rawEvent) {
 
@@ -16,32 +20,82 @@ module.exports = function(rawEvent) {
     async.waterfall([
 
       function getOrCreateUser(callback) {
-        SeedAppService.getOrCreateUser(rawEvent).then(function(){
-          callback(null, rawEvent);
+        Raven.context(function() {
+          Raven.captureBreadcrumb({
+            message: 'Contacting Raheem API to get or create new user',
+            category: 'API',
+            data: {
+              fbID: senderID,
+              rawEvent: rawEvent
+            }
+          });
+          SeedAppService.getOrCreateUser(rawEvent).then(function(){
+            callback(null, rawEvent);
+          });
         });
       },
 
       function parseMessage(messagingEvent, callback) {
-        MessageService.parse(messagingEvent).then(function(parsedMessage){
-          callback(null, parsedMessage);
+        Raven.context(function() {
+          Raven.captureBreadcrumb({
+            message: 'Parsing incoming message',
+            category: 'Message Services',
+            data: {
+              fbID: senderID,
+              rawEvent: messagingEvent
+            }
+          });
+          MessageService.parse(messagingEvent).then(function(parsedMessage){
+            callback(null, parsedMessage);
+          });
         });
       },
 
       function courierIn(parsedMessage, callback) {
-        MessageCourier.in(parsedMessage, senderID).then(function(digestedMessage){
-          callback(null, digestedMessage);
+        Raven.context(function() {
+          Raven.captureBreadcrumb({
+            message: 'Ingesting incoming message',
+            category: 'Message Engine',
+            data: {
+              fbID: senderID,
+              rawEvent: parsedMessage
+            }
+          });
+          MessageCourier.in(parsedMessage, senderID).then(function(digestedMessage){
+            callback(null, digestedMessage);
+          });
         });
       },
 
       function courierOut(digestedMessage, callback) {
-        MessageCourier.out(digestedMessage, senderID).then(function(newMessage){
-          SeedAppService.logIncidentData(senderID);
-          callback(null, newMessage);
+        Raven.context(function() {
+          Raven.captureBreadcrumb({
+            message: 'Formatting outgoing message',
+            category: 'Message Engine',
+            data: {
+              fbID: senderID,
+              rawEvent: digestedMessage
+            }
+          });
+          MessageCourier.out(digestedMessage, senderID).then(function(newMessage){
+            SeedAppService.logIncidentData(senderID);
+            callback(null, newMessage);
+          });
         });
       }],
 
       function(err, newMessage) {
-        MessageService.formatAndSend(newMessage, senderID)
+        Raven.context(function() {
+          Raven.captureBreadcrumb({
+            message: 'Formatting and sending outgoing message',
+            category: 'Message Services',
+            data: {
+              fbID: senderID,
+              rawEvent: newMessage
+            }
+          });
+          MessageService.formatAndSend(newMessage, senderID)
+        });
       }
     );
   }
